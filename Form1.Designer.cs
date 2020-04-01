74a6153762307b7fc6fe53a5a986dd18a2e35537
Teodor <teodor.tkaczyk0407@gmail.com>
2020-04-01 23:31:14 +0200

    Push to github repository

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PC = new System.Windows.Forms.Button();
+            this.PLAYERS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PC
+            // 
+            this.PC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PC.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PC.Location = new System.Drawing.Point(95, 161);
+            this.PC.Name = "PC";
+            this.PC.Size = new System.Drawing.Size(484, 218);
+            this.PC.TabIndex = 0;
+            this.PC.Text = "Play with PC";
+            this.PC.UseVisualStyleBackColor = true;
+            this.PC.Click += new System.EventHandler(this.PC_Click);
+            // 
+            // PLAYERS
+            // 
+            this.PLAYERS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PLAYERS.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PLAYERS.Location = new System.Drawing.Point(756, 161);
+            this.PLAYERS.Name = "PLAYERS";
+            this.PLAYERS.Size = new System.Drawing.Size(484, 218);
+            this.PLAYERS.TabIndex = 1;
+            this.PLAYERS.Text = "Play on 2 players";
+            this.PLAYERS.UseVisualStyleBackColor = true;
+            this.PLAYERS.Click += new System.EventHandler(this.PLAYERS_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 633);
+            this.Controls.Add(this.PLAYERS);
+            this.Controls.Add(this.PC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PC;
+        private System.Windows.Forms.Button PLAYERS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        private Form board;
+        private Button[] fields = new Button[9];
+        private string sign = "";
+        private string player = "";
+        private Label playerWhich;
+        private Label gameProgress;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void disableFields()
+        {
+            for (int i = 0; i < fields.Length; i++)
+            {
+                fields[i].Enabled = false;
+            }
+        }
+
+        private void win(string signCopy, string playerCopy)
+        {
+            bool winner = false;
+            if ((fields[1].Text == fields[0].Text && fields[2].Text == fields[1].Text && fields[1].Text != "")
+                || (fields[4].Text == fields[3].Text && fields[5].Text == fields[4].Text && fields[3].Text != "")
+                || (fields[7].Text == fields[6].Text && fields[8].Text == fields[7].Text && fields[6].Text != ""))
+            {
+                gameProgress.Text += "\n\nAnd the winner is: " + playerCopy + " who wins putting " + signCopy + " in a row";
+                winner = true;
+            }
+            else if ((fields[3].Text == fields[0].Text && fields[6].Text == fields[3].Text && fields[3].Text != "")
+                || (fields[4].Text == fields[1].Text && fields[7].Text == fields[4].Text && fields[4].Text != "")
+                || (fields[5].Text == fields[2].Text && fields[8].Text == fields[5].Text && fields[2].Text != ""))
+            {
+                gameProgress.Text += "\n\nAnd the winner is: " + playerCopy + " who wins putting " + signCopy + " in a column";
+                winner = true;
+            }
+            else if ((fields[4].Text == fields[0].Text && fields[8].Text == fields[4].Text && fields[4].Text != "")
+                || (fields[2].Text == fields[4].Text && fields[6].Text == fields[4].Text && fields[2].Text != ""))
+            {
+                gameProgress.Text += "\n\nAnd the winner is: " + playerCopy + " who wins putting " + signCopy + " diagonally";
+                winner = true;
+            }
+            if (winner) {
+                disableFields();
+                playerWhich.Text = "GAME OVER";
+                player = "";
+            }
+            else
+            {
+                bool draw = true;
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    if (fields[i].Text == "")
+                    {
+                        draw = false;
+                        break;
+                    }
+                }
+                if (draw)
+                {
+                    disableFields();
+                    gameProgress.Text += "\n\nDraw!";
+                    playerWhich.Text = "GAME OVER";
+                    player = "";
+                }
+                else
+                {
+                    playerWhich.Text = "Turn: " + player + " - " + sign;
+                    if (player == "Computer" || player == "Player")
+                    {
+                        insertSignPCPlayer();
+                    }
+                }
+            }
+        }
+
+        private void insertSign(Button button, string player1, string player2)
+        {
+            string signCopy = sign, playerCopy = player;
+            if (button.Text == "")
+            {
+                gameProgress.Text += "\n" + player + " placed: " + signCopy;
+                if (player == player1) player = player2;
+                else player = player1;
+            }
+            if (sign == "O" && button.Text == "")
+            {
+                sign = "X";
+                button.Text = "O";
+            }
+            else if (sign == "X" && button.Text == "")
+            {
+                sign = "O";
+                button.Text = "X";
+            }
+            if (player != "") win(signCopy, playerCopy);
+        }
+
+        private void insertSign2Players(Object sender, EventArgs e, string player1, string player2)
+        {
+            Button button = (Button)sender;
+            insertSign(button, player1, player2);
+        }
+
+        private int closeToWin(string SIGN)
+        {
+            if (fields[0].Text == fields[1].Text && fields[0].Text == SIGN && fields[2].Text == "") return 2;
+            if (fields[0].Text == fields[2].Text && fields[0].Text == SIGN && fields[1].Text == "") return 1;
+            if (fields[1].Text == fields[2].Text && fields[1].Text == SIGN && fields[0].Text == "") return 0;
+            if (fields[3].Text == fields[4].Text && fields[3].Text == SIGN && fields[5].Text == "") return 5;
+            if (fields[3].Text == fields[5].Text && fields[3].Text == SIGN && fields[4].Text == "") return 4;
+            if (fields[4].Text == fields[5].Text && fields[4].Text == SIGN && fields[3].Text == "") return 3;
+            if (fields[6].Text == fields[7].Text && fields[6].Text == SIGN && fields[8].Text == "") return 8;
+            if (fields[6].Text == fields[8].Text && fields[6].Text == SIGN && fields[7].Text == "") return 7;
+            if (fields[7].Text == fields[8].Text && fields[7].Text == SIGN && fields[6].Text == "") return 6;
+            if (fields[0].Text == fields[3].Text && fields[0].Text == SIGN && fields[6].Text == "") return 6;
+            if (fields[0].Text == fields[6].Text && fields[0].Text == SIGN && fields[3].Text == "") return 3;
+            if (fields[3].Text == fields[6].Text && fields[3].Text == SIGN && fields[0].Text == "") return 0;
+            if (fields[1].Text == fields[4].Text && fields[1].Text == SIGN && fields[7].Text == "") return 7;
+            if (fields[1].Text == fields[7].Text && fields[1].Text == SIGN && fields[4].Text == "") return 4;
+            if (fields[4].Text == fields[7].Text && fields[4].Text == SIGN && fields[1].Text == "") return 1;
+            if (fields[2].Text == fields[5].Text && fields[2].Text == SIGN && fields[8].Text == "") return 8;
+            if (fields[2].Text == fields[8].Text && fields[2].Text == SIGN && fields[5].Text == "") return 5;
+            if (fields[5].Text == fields[8].Text && fields[5].Text == SIGN && fields[2].Text == "") return 2;
+            if (fields[0].Text == fields[4].Text && fields[0].Text == SIGN && fields[8].Text == "") return 8;
+            if (fields[0].Text == fields[8].Text && fields[0].Text == SIGN && fields[4].Text == "") return 4;
+            if (fields[4].Text == fields[8].Text && fields[4].Text == SIGN && fields[0].Text == "") return 0;
+            if (fields[2].Text == fields[4].Text && fields[2].Text == SIGN && fields[6].Text == "") return 6;
+            if (fields[2].Text == fields[6].Text && fields[2].Text == SIGN && fields[4].Text == "") return 4;
+            if (fields[4].Text == fields[6].Text && fields[4].Text == SIGN && fields[2].Text == "") return 2;
+            return -1;
+        }
+
+        private void PCMove(string playerSign)
+        {
+            if (closeToWin(sign) != -1) insertSign(fields[closeToWin(sign)], "Player", "Computer");
+            else if (closeToWin(playerSign) != -1) insertSign(fields[closeToWin(playerSign)], "Player", "Computer");
+            else
+            {
+                bool inserted = false;
+                do
+                {
+                    int random = new Random().Next(0, 9);
+                    if (fields[random].Text == "")
+                    {
+                        insertSign(fields[random], "Player", "Computer");
+                        inserted = true;
+                    }
+                } while (!inserted);
+            }
+        }
+
+        private void insertSignPCPlayer()
+        {
+            if (player == "Player")
+            {
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    fields[i].Click += new EventHandler((sender, e) => insertSign2Players(sender, e, "Player", "Computer"));
+                }
+            }
+            else if (player == "Computer")
+            {
+                string playerSign = "";
+                if (sign == "X") playerSign = "O";
+                else playerSign = "X";
+                PCMove(playerSign);
+            }
+        }
+
+        private void initializeGame()
+        {
+            if (new Random().Next(0, 2) == 0) sign = "O";
+            else sign = "X";
+            board = new Form();
+            board.Size = new Size(1000, 680);
+            int marginTop = 1, marginLeft = 1;
+            for (int i = 0; i < 9; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(80, 80);
+                if (i % 3 == 0)
+                {
+                    marginTop++;
+                    marginLeft = 1;
+                }
+                button.Top = marginTop * 80 - 80;
+                button.Left = marginLeft * 80;
+                button.Cursor = Cursors.Hand;
+                button.Font = new Font("Arial", 36.0f);
+                board.Controls.Add(button);
+                fields[i] = button;
+                marginLeft++;
+            }
+            playerWhich = new Label();
+            playerWhich.Top = 80;
+            playerWhich.Left = 400;
+            playerWhich.Font = new Font("Arial", 12.0f);
+            playerWhich.Height = 50;
+            playerWhich.Width = 400;
+            board.Controls.Add(playerWhich);
+            gameProgress = new Label();
+            gameProgress.Top = 130;
+            gameProgress.Left = 400;
+            gameProgress.Font = new Font("Arial", 12.0f);
+            gameProgress.Height = 400;
+            gameProgress.Width = 400;
+            gameProgress.Text = "Progress: ";
+            board.Controls.Add(gameProgress);
+            board.Show();
+        }
+
+        private void gameMode(string player1, string player2)
+        {
+            if (new Random().Next(0, 2) == 0) player = player1;
+            else player = player2;
+            playerWhich.Text += "Turn: " + player + " - " + sign;
+        }
+
+        private void PLAYERS_Click(object sender, EventArgs e)
+        {
+            initializeGame();
+            gameMode("Player 1", "Player 2");
+            for (int i = 0; i < fields.Length; i++)
+            {
+                fields[i].Click += new EventHandler((sender2, e2) => insertSign2Players(sender2, e2, "Player 1", "Player 2"));
+            }
+        }
+
+        private void PC_Click(object sender, EventArgs e)
+        {
+            initializeGame();
+            gameMode("Player", "Computer");
+            insertSignPCPlayer();
+        }
+    }
+}
